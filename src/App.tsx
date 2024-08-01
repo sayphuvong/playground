2cebca4c9c918b3beccae412ffab2d8737802fe3
Safu <phusv@inspirelab.io>
2024-08-01 16:47:55 +0700

    Add more page to testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import AboutPage from "./pages/About";
+import VideoPage from "./pages/Video";
+import HomePage from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <header className="App-header">
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/video">Video</Link>
+          </nav>
+        </header>
+
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/video" element={<VideoPage />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import logo from "../logo.svg";
+
+export default function HomePage() {
+  return (
+    <div>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Deploy ID: 1722504774059</p>
+    </div>
+  );
+}
